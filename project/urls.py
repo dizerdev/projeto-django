38ffa95ed38bac2cb138fda4314c7236f9596916
Diego Zerbini <zerbiniprogramador@gmail.com>
2024-01-03 10:03:29 -0300

    Explore include function for nesting URLs

--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for projeto project.
+URL configuration for project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.http import HttpResponse
+
+def my_view(request):
+    return HttpResponse('Teste')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('ecommerce.urls')),
+    path('test', include('ecommerce.urls')),
 ]
