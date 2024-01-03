90aad5d3545bc60b4dfd1baee9d5a2413a40587b
Diego Zerbini <zerbiniprogramador@gmail.com>
2024-01-03 17:21:47 -0300

    Set :root environment styles and build header component

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'layout/home.html', context={
+    return render(request, 'pages/home/home.html', context={
         'name':'Diego',
         })
