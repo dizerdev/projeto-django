6709c34a31da6ef190992917da8e499f93449e49
Diego Zerbini <zerbiniprogramador@gmail.com>
2024-01-03 21:56:17 -0300

    Build html components: product, search, header

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'pages/home/home.html', context={
-        'name':'Diego',
+        'name':'Ecommerce',
         })
