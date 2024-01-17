bc0a9e41eb217ad7ecc273c36086f9bad9daa404
Diego Zerbini <zerbiniprogramador@gmail.com>
2024-01-16 23:21:26 -0300

    Create an Factory to fake data and inject data on product component

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,13 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from utils.product.factory import make_product
 # Create your views here.
+
 
 def home(request):
     return render(request, 'pages/home/home.html', context={
         'name':'Ecommerce',
+        'products': [make_product() for _ in range(10)]
         })
+
 
 def product(request, id):
     return render(request, 'pages/details/details.html', context={
         'name':'Ecommerce',
+        'product': make_product(),
         })
+
